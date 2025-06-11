5b28b5f57ce78cfb621c14c4bd258b4ff6ffebbe
Srilekha Gopanaboina <69714789+srilekhagopanaboina@users.noreply.github.com>
2025-06-11 05:50:01 -0500

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,14 +1,27 @@
 export const experiences = [
   {
     id: 1,
+    title: "Software Engineer",
+    company: "Around mee app",
+    duration: "(May 2023 - May 2025)",
+  },
+  {
+    id: 2,
     title: "Associate Software Engineer",
     company: "Accenture",
     duration: "(May 2021 - May 2023)",
   },
   {
-    id: 2,
-    title: "Software Engineer",
-    company: "Around mee app",
-    duration: "(May 2023 - May 2025)",
+    id: 3,
+    title: "Software Development and IT Specialist ",
+    company: "Saint Louis University",
+    duration: "(Dec 2024 - May 2025)",
   },
+  {
+    id: 4,
+    title: "Database Teaching Assistant",
+    company: "Saint Louis University",
+    duration: "(Aug 2024 - Dec 2024)",
+  },
+  
 ];
