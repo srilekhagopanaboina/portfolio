5ab482e19f432af7d5cc5d4c0e7e78914126952a
Srilekha Gopanaboina <srilekhagopanaboina@gmail.com>
2025-09-19 14:26:54 -0500

    Update educations data structure
    Removed duration from education entries.

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -2,19 +2,12 @@ export const educations = [
   {
     id: 1,
     title: "Master of Science in Computer Information Systems",
-    duration: "2023 - 2025",
     institution: "Saint Louis University",
   },
   {
     id: 2,
     title: "Bachelor of Science in Computer Science Engineering",
-    duration: "2017 - 2021",
     institution: "JNTUH",
   },
-  {
-    id: 3,
-    title: "Senior Secondary Education",
-    duration: "2015 - 2017",
-    institution: "Sri Chaitanya Educational Institute",
-  },
 ];
+
